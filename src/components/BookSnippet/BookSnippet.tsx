6d4631b5fd896f3737be43d047e2f734211f3b27
Nastez <mineevanastez@gmail.com>
2021-06-03 12:11:12 +0300

    Added another image for cases with books without covers. Added cross icon for close modal window

--- src/components/BookSnippet/BookSnippet.tsx
+++ src/components/BookSnippet/BookSnippet.tsx
@@ -43,7 +43,6 @@ const BookSnippet: React.FC<PropsType> = ({onItemClick}) => {
             ))}
         </div>
     </>
-
 }
 
 export default BookSnippet
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -10,38 +10,30 @@ export type ModalDataType = {
 
 const Modal: React.FC<ModalDataType> = ({bookDataPropsType, onHide}) => {
 
-    const authorNameArr = bookDataPropsType.authorName === undefined || bookDataPropsType.authorName === null ?
-        <span>No name</span> : bookDataPropsType.authorName.join(', ')
-
     return <>
-        <div>
-            <div className={s.modal}>
-                <h1>{bookDataPropsType.title}</h1>
-                <div>ISBN: {bookDataPropsType.isbn === undefined || bookDataPropsType.isbn === null ?
-                    <div>No ISBN</div> : bookDataPropsType.isbn[0]}</div>
-            </div>
-            <div className={s.modal}>
-                <div className={s.modalBox}>
-                    <div>
-                        {bookDataPropsType.isbn === undefined || bookDataPropsType.isbn === null ?
-                            <img alt='No cover' src={bookWithoutCover} className={s.bookWithoutCover}/> :
-                            <img alt='No cover'
-                                 src={`https://covers.openlibrary.org/b/isbn/${bookDataPropsType.isbn[0]}-L.jpg`}/>}
-                    </div>
-                    <div className={s.modalText}>
-                        <div>Title: {bookDataPropsType.title}</div>
-                        <div className={s.modalAuthorNameText}>Author Name: {authorNameArr}</div>
-                        <div>Publish
-                            Date: {bookDataPropsType.publishDate === undefined || bookDataPropsType.publishDate === null ?
-                                <div>No Publish Date</div> : bookDataPropsType.publishDate[0]}</div>
-                        <div>Publish
-                            Date: {bookDataPropsType.publisher === undefined || bookDataPropsType.publisher === null ?
-                                <div>No Publisher</div> : bookDataPropsType.publisher[0]}</div>
-                        <div>ISBN: {bookDataPropsType.isbn === undefined || bookDataPropsType.isbn === null ?
-                            <div>No ISBN</div> : bookDataPropsType.isbn[0]}</div>
-                    </div>
+        <div className={s.modal}>
+            <div onClick={onHide} className={s.onHideButton}></div>
+            <div className={s.modalBox}>
+                <div>
+                    {bookDataPropsType.isbn === undefined || bookDataPropsType.isbn === null ?
+                        <img alt='No cover' src={bookWithoutCover} className={s.bookWithoutCover}/> :
+                        <img alt='No cover'
+                             src={`https://covers.openlibrary.org/b/isbn/${bookDataPropsType.isbn[0]}-L.jpg`}/>}
                 </div>
-                <button onClick={onHide}>Hide</button>
+                <div className={s.modalText}>
+                    <div>Title: {bookDataPropsType.title}</div>
+                    <div>Author
+                        Name: {bookDataPropsType.authorName === undefined || bookDataPropsType.authorName === null ?
+                            <span>No name</span> : bookDataPropsType.authorName.join(', ')}</div>
+                    <div>Publish
+                        Date: {bookDataPropsType.publishDate === undefined || bookDataPropsType.publishDate === null ?
+                            <div>No Publish Date</div> : bookDataPropsType.publishDate[0]}</div>
+                    <div>Publish
+                        Date: {bookDataPropsType.publisher === undefined || bookDataPropsType.publisher === null ?
+                            <div>No Publisher</div> : bookDataPropsType.publisher[0]}</div>
+                    <div>ISBN: {bookDataPropsType.isbn === undefined || bookDataPropsType.isbn === null ?
+                        <div>No ISBN</div> : bookDataPropsType.isbn[0]}</div>
+                </div>
             </div>
 
         </div>
